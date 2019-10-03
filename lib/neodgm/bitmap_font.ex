450eb773aa73f2aa56855a853e4a5a3d757bc090
Dalgona <dalgona@hontou.moe>
2019-10-03 22:28:21 +0900

    feat(neodgm_pro): enable "pro" glyph sources variant

--- lib/neodgm/bitmap_font.ex
+++ lib/neodgm/bitmap_font.ex
@@ -1,23 +1,34 @@
 defmodule NeoDGM.BitmapFont do
-  base_glyph_sources =
+  common_glyph_sources =
     [
-      BasicLatin,
       BlockElements,
       BoxDrawing,
       BraillePatterns,
       Dingbats,
       EnclosedAlphanumerics,
-      GeneralPunctuation,
       GeometricShapes,
       HangulCompatibilityJamo,
       HangulJamo,
       HangulJamoSource,
       HangulSyllables,
-      Latin1Supplement,
-      PowerlineSymbols,
       Trigrams,
       Components.ShortCapitals,
       Components.Diacritics
+    ]
+    |> Enum.map(&Module.concat(__MODULE__, &1))
+
+  base_glyph_sources =
+    [
+      BasicLatin,
+      GeneralPunctuation,
+      Latin1Supplement,
+      PowerlineSymbols
+    ]
+    |> Enum.map(&Module.concat(__MODULE__, &1))
+
+  pro_glyph_sources =
+    [
+      Pro.BasicLatin
     ]
     |> Enum.map(&Module.concat(__MODULE__, &1))
 
@@ -34,10 +45,23 @@ defmodule NeoDGM.BitmapFont do
 
   def get_sources("code") do
     [
+      unquote_splicing(common_glyph_sources),
       unquote_splicing(base_glyph_sources),
       unquote_splicing(ligature_glyph_sources)
     ]
   end
 
-  def get_sources(_), do: unquote(base_glyph_sources)
+  def get_sources("pro") do
+    [
+      unquote_splicing(common_glyph_sources),
+      unquote_splicing(pro_glyph_sources)
+    ]
+  end
+
+  def get_sources(_) do
+    [
+      unquote_splicing(common_glyph_sources),
+      unquote_splicing(base_glyph_sources)
+    ]
+  end
 end
